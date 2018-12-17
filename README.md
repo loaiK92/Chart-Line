f04a001f1167daeb6b6c2617e48b3edb57a2d05c
loaikahel1992 <loaikahel723@gmail.com>
2018-12-17 13:54:19 +0100

    first upload

--- README.md
+++ README.md
@@ -1,27 +1,29 @@
-# HomeLike-Chat-App
+# Chart-Line
 
-This project is an assignment for **HOMELIKE INTERNET GmbH** which is chat application based on `react.js`, `node.js` and `mongoDB`
+This project is a simple single page web app which allows the user to plot of all the samples of a single variable in a simple line chart.
+The user is able to select and switch the plotted variable by its name from a simple text input box.
 
-### Implemented requirements :
-   - Chat list:
-        - list chats in which you are participant (there can be multiple participants in a chat)
-        - clicking on an item in the chat list will take you to the overview of the chat details
-   - Chat detail overview:
-        - list all messages in the chat.
-        - functionality to send a new chat message
+## Data
 
-### Implemented extensions :
-   - Build a profile page for each user
-   - Allow editing and deleting of messages
+The data is containing 2000 data samples (row) & 200 variables, where the first row contains the variable names.
+Datastore structure: my choice for the data storage was MongoDB, due to its fast response time.
+The data is stored using mLab database service and structured as one data collection which contains 2000 documents consisting of 200 fields [var001 ... var200].
+
+### Technologies
+
+- The backend was built with Node.js using Express as my web framework and Mongoose library.
+- For the user interface I used React.js, and Apexchart for the data visualization, SCSS preprocessor.
+- Note: because the response was taking over 3s sometimes on a slow network I used streams to optimize the user experience. Now the user is able to see a result within 300ms while the chart is being populated gradually.
 
 ## Getting Started :
 
-1- Clone this repo `git clone https://github.com/loaiK92/HomeLike-Chat-App.git`
+1- Clone this repo `git clone https://github.com/loaiK92/Chat-App.git`
 
-2- Create 'variables.env' file in the folder route which should contain : `MongoDB URI` & `port`
-   - Example:
-        - `DATABASE = mongodb://<dbuser>:<dbpassword>@ds254215.mlab.com:254215/database name`
-        - `PORT = 8000`
+2- Create `variables.env` file in the folder route which should contain : `MongoDB URI` & `port`
+
+- Example:
+  - `DATABASE = mongodb://<dbuser>:<dbpassword>@ds254215.mlab.com:254215/database name`
+  - `PORT = 8000`
 
 3- Install dependencies `npm install`
 
--- server/server.js
+++ server/server.js
@@ -1,14 +1,9 @@
 const app = require("express")();
 const http = require("http").Server(app);
-const path = require("path");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
-const io = require("socket.io")(http);
+const JSONStream = require("JSONStream"); // use JSONStream to get data as a stream
 require("dotenv").config({ path: "./variables.env" });
-
-// database models
-const UserProfile = require("../models/UserProfileModel");
-const ChatGroup = require("../models/ChatGroupModel");
 
 // Enable Cross Origing Rescource sharing
 // p.s. this code is copied from CROS on ExpressJS
@@ -30,119 +25,6 @@ mongoose.connect(
   { useNewUrlParser: true }
 );
 
-// import router files and use it
-// Note: each component file in "src/components" has its own router file here
-const indexRoute = require("./routes/indexRoute");
-const signupRoute = require("./routes/signupRoute");
-const loginRoute = require("./routes/loginRoute");
-const userProfileRoute = require("./routes/userProfileRoute");
-const creategroupRoute = require("./routes/creategroupRoute");
-const chatGroupRoute = require("./routes/chatGroupRoute");
-
-app.use("/", indexRoute);
-app.use("/signup", signupRoute);
-app.use("/login", loginRoute);
-app.use("/profile", userProfileRoute);
-app.use("/create-chat-group", creategroupRoute);
-app.use("/group", chatGroupRoute);
-
-// empty object to update online user
-const onlineUser = {};
-
-// listen to a connection event on the server side
-io.on("connection", socket => {
-  // login event happen when the user open any chat group
-  socket.on("login", user => {
-    // add online user to the empty object
-    onlineUser[socket.id] = user.userID;
-    // update user profile with status: true
-    UserProfile.updateOne({ username: user.name }, { status: true })
-      .then(data => {
-        return;
-      })
-      .catch(err => console.log(err));
-    // then emit the event
-    io.emit("login", user);
-  });
-
-  // listen for a disconnect
-  socket.on("disconnect", () => {
-    // update user profile with status: false
-    UserProfile.updateOne({ _id: onlineUser[socket.id] }, { status: false })
-      .then(data => {
-        return;
-      })
-      .catch(err => console.log(err));
-
-    // then emit the event
-    io.emit("disconnect", onlineUser[socket.id]);
-  });
-
-  // listen for delete message event
-  socket.on("delete message", data => {
-    // delete the message from the chat group
-    ChatGroup.updateOne(
-      { fakeID: data.groupID },
-      { $pull: { messages: { msgFakeID: data.msgFakeID } } }
-    )
-      .then(data => {
-        return;
-      })
-      .catch(err => console.log(err));
-    // then emit the event
-    io.emit("delete message", data);
-  });
-
-  // listen for edit message event
-  socket.on("edit message", data => {
-    const messagesArray = [];
-    // get chat group data
-    ChatGroup.find({ fakeID: data.groupID })
-      .then(result => {
-        // check the messages
-        result[0].messages.map(item => {
-          // if find the message ID, update this message content
-          if (item.msgFakeID === data.msgID) {
-            item.content = data.message;
-          }
-          // push the item to the new array "messagesArray"
-          messagesArray.push(item);
-        });
-        // then set the group messages to the new array
-        ChatGroup.updateOne(
-          { fakeID: data.groupID },
-          { messages: messagesArray }
-        )
-          .then(res => {
-            return;
-          })
-          .catch(err => console.log(err));
-      })
-      .catch(err => console.log(err));
-    // then emit the event
-    io.emit("edit message", data);
-  });
-
-  // listen for a "chat message" event
-  socket.on("chat message", message => {
-    // save the messages in database
-    ChatGroup.updateOne(
-      { fakeID: message.groupId },
-      { $push: { messages: message } }
-    ).then(data => {
-      return;
-    });
-    // then emit the event
-    io.emit("chat message", message);
-  });
-
-  //Listen to a user typing
-  socket.on("typing", data => {
-    io.emit("typing", data);
-    console.log(data);
-  });
-});
-
 // connect to database
 const db = mongoose.connection;
 db.on("error", err => {
@@ -150,6 +32,28 @@ db.on("error", err => {
 });
 db.once("open", () => {
   console.log(`Database connection is open!!!`);
+});
+
+// create DB collection
+const DataModel = db.collection("data");
+
+// request to get data from database
+app.get("/:variable", (req, res) => {
+  // get the params value which is variable name
+  const variable = req.params.variable;
+
+  // create option object
+  const options = { _id: 0 }; // to get all document regardless of their IDs
+
+  // then add the variable name as a property to the option object with value true
+  options[variable] = true; // to get all fields which has the variable name
+
+  // search for the variable values in database
+  DataModel.find()
+    .project(options) // project option
+    // .limit(300)
+    .pipe(JSONStream.stringify()) // stringify the data using JSONStream
+    .pipe(res.type("json")); // send data with type json
 });
 
 // run the server
